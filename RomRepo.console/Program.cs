31729aa8f9508519ffa9fa9daafa8e8f20c3bfd2
Shawn Renner <shawn.renner@gmail.com>
2023-08-12 17:05:48 -0500

    create console service

---
+++ RomRepo.console/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace RomRepo.console
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("RomRepo.console is starting up at " + DateTime.Now.ToLongTimeString());
+
+            await Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    var builder = new ConfigurationBuilder().AddJsonFile($"appsettings.json", true, true);
+                    var config = builder.Build();
+                    services.AddHostedService<RomRepoHostedService>();
+                    services.AddOptions();
+                })
+                .RunConsoleAsync();
+            Console.WriteLine("Shutting down at " + DateTime.Now.ToLongTimeString());
+        }
+    }
+}
---
+++ RomRepo.console/RomRepoHostedService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomRepo.console
+{
+    internal sealed class RomRepoHostedService : IHostedService
+    {
+        private readonly ILogger _logger;
+        private readonly IHostApplicationLifetime _appLifetime;
+        
+
+        public RomRepoHostedService(ILogger<RomRepoHostedService> logger, IHostApplicationLifetime appLifetime)
+        {
+            _logger = logger;
+            _appLifetime = appLifetime;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _appLifetime.ApplicationStarted.Register(() =>
+            {
+                Task.Run(async () =>
+                {
+                    while(true)
+                    {
+                        //do the bartman
+                        await Task.Delay(100);
+                    }
+                });
+            });
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+    }
+
+
+}
