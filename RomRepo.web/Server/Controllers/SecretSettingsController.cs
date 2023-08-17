8c360db439ac46a3ae297e52a78fbd3d493225f6
Shawn Renner <shawn.renner@gmail.com>
2023-08-17 07:10:38 -0500

    user secret setting mgmt

---
+++ RomRepo.web/Server/Controllers/SecretSettingsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RomRepo.web.Server.Controllers
+{
+    public class SecretSettingsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+
+        }
+
+        [HttpGet("/setting/romroot")]
+        public bool GetRomRoot()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RomRepo.web/Server/SecretSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace RomRepo.web.Server
+{
+    public class SecretSettings
+    {
+        public string RomRootFolder { get; set; }
+        public string UniqueIdentifier { get; set; }
+    }
+}
