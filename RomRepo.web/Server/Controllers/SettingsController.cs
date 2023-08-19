c1950f61c6bbee23e707a437f193a4c7d76dcd91
Shawn Renner <shawn.renner@gmail.com>
2023-08-18 23:14:17 -0500

    put folder settings in json

---
+++ RomRepo.web/Server/Controllers/SettingsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RomRepo.web.Server.Controllers
+{
+    public class SettingsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+
+        }
+
+        [HttpGet("api/setting/romroot")]
+        public string GetRomRoot()
+        {
+            //todo get from settings file
+            return "\\\\mister\\sdcard\\games";
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("api/setting/saveroot")]
+        public bool GetSaveRoot()
+        { 
+            throw new NotImplementedException(); 
+        }
+
+        [HttpGet("api/setting/savestateroot")]
+        public bool GetSaveStateRoot()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
