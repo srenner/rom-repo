bfe87dcb5d998a666abf08416304d9588d9a10ee
Shawn Renner <shawn.renner@gmail.com>
2023-10-24 20:48:42 -0500

    docker support

--- RomRepo.api.dat/Models/NotMapped/ApiRequest.cs
+++ RomRepo.api.dat/Models/NotMapped/ApiRequest.cs
@@ -1,5 +1,8 @@
 ﻿namespace RomRepo.api.dat.Models.NotMapped
 {
+    /// <summary>
+    /// Input parameters from a client requesting a new API Key. Client must include at least one parameter to receive a key.
+    /// </summary>
     public class ApiRequest
     {
         public string InstallationID { get; set; }
