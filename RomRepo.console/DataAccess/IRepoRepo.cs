2bd3b89d366923029cfd36ca50303c4a852bce51
Shawn Renner <shawn.renner@gmail.com>
2023-10-26 07:23:48 -0500

    start repository

---
+++ RomRepo.console/DataAccess/IRepoRepo.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomRepo.console.DataAccess
+{
+    public interface IRepoRepo
+    {
+        void GetAllCores();
+        void GetCore(int coreID);
+        void UpdateCore(int coreID);
+
+        void GetRomsForCore(int coreID);
+        void GetRom(int romID);
+        void UpdateRom(int romID);
+
+
+        void GetSystemSettings();
+        void AddSystemSetting();
+        void UpdateSystemSetting();
+
+
+    }
+}
---
+++ RomRepo.console/DataAccess/RepoRepo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomRepo.console.DataAccess
+{
+    public class RepoRepo : IRepoRepo
+    {
+        public void AddSystemSetting()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetAllCores()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetCore(int coreID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetRom(int romID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetRomsForCore(int coreID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetSystemSettings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCore(int coreID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateRom(int romID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSystemSetting()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
