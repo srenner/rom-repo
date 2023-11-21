a2f17a2c39a9ef8c85cd098ac6fbf7fb8c3a5085
Shawn Renner <shawn.renner@gmail.com>
2023-11-20 20:26:18 -0600

    system setting api endpoints

---
+++ RomRepo.service/Controllers/SystemSettingController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RomRepo.console.Controllers;
+using RomRepo.console.DataAccess;
+using RomRepo.console.Models;
+using RomRepo.console.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomRepo.service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SystemSettingController : ControllerBase
+    {
+        private ILogger<SystemSettingController> _logger;
+        private readonly IClientRepo _repo;
+
+        public SystemSettingController(ILogger<SystemSettingController> logger, IClientRepo repo)
+        {
+            _logger = logger;
+            _repo = repo;
+        }
+
+
+        [HttpGet("{name}")]
+        public async Task<SystemSetting> GetSystemSetting(string name)
+        {
+            //_repo.GetSystemSetting()
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<SystemSetting>> GetSystemSettings()
+        {
+
+            return await _repo.GetSystemSettings();
+        }
+
+    }
+}
