7a3923455315237b7bb2556f07d5c13bcf7ba12e
Shawn Renner <shawn.renner@gmail.com>
2023-11-25 15:23:32 -0600

    ui update

--- RomRepo.web/src/App.vue
+++ RomRepo.web/src/App.vue
@@ -1,5 +1,8 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+
+
+
 </script>
 
 <template>
@@ -7,15 +10,19 @@ import { RouterLink, RouterView } from 'vue-router'
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
       <ul class="nav nav-pills">
         <li class="nav-item">
-          <RouterLink class="nav-link" to="/" title="Systems"><i class="bi bi-pc-display-horizontal"></i></RouterLink>
+          <RouterLink class="nav-link" to="/" title="Systems"><i class="bi bi-pc-display-horizontal highlight-link"></i></RouterLink>
         </li>
         <li class="nav-item">
-          <RouterLink class="nav-link" to="/about" title="Roms"><i class="bi bi-controller"></i></RouterLink>
+          <RouterLink class="nav-link" to="/about" title="Roms"><i class="bi bi-controller highlight-link"></i></RouterLink>
         </li>
         <li class="nav-item">
-          <RouterLink class="nav-link" to="/about" title="Settings"><i class="bi bi-gear"></i></RouterLink>
+          <RouterLink class="nav-link" to="/about" title="Settings"><i class="bi bi-gear highlight-link"></i></RouterLink>
+        </li>
+        <li class="nav-item">
+          <i title="System Status" class="nav-link bi bi-stoplights text-success"></i>
         </li>
       </ul>
+      
     </header>
     <div>
       <RouterView />
