c98cde3b4e8b462f70bda575d987b90905daf048
Shawn Renner <shawn.renner@gmail.com>
2024-03-26 20:49:18 -0500

    stub favorite api

---
+++ RomRepo.api/Controllers/FavoriteController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RomRepo.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FavoriteController : ControllerBase
+    {
+        [HttpPost("game")]
+        public void ToggleGameFavorite(int gameID)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("gamesystem")]
+        public void ToggleGameSystemFavorite(int gameSystemID)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
