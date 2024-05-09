caf899367a9dfa67eb4490d5cc6050b9fc72d9e0
Shawn Renner <shawn.renner@gmail.com>
2024-05-09 08:47:05 -0500

    use swagger on release build

--- server/RomRepo.api/Program.cs
+++ server/RomRepo.api/Program.cs
@@ -24,20 +24,11 @@ namespace RomRepo.api
             builder.Services.Configure<KestrelServerOptions>(options => options.Limits.MaxRequestBodySize = int.MaxValue);
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
+            app.UseSwagger();
+            app.UseSwaggerUI();
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
-
             app.Run();
         }
     }
