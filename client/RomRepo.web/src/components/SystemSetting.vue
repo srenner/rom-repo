ae6215450170580d936150becf263779753b15f1
Shawn Renner <shawn.renner@gmail.com>
2024-05-12 21:00:19 -0500

    begin client settings page design

---
+++ client/RomRepo.web/src/components/SystemSetting.vue
@@ -0,0 +1,18 @@
+<script setup>
+const props = defineProps({
+  setting: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+    <div>
+        <div>{{ setting.name }}</div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- client/RomRepo.web/src/views/SettingsView.vue
+++ client/RomRepo.web/src/views/SettingsView.vue
@@ -1,8 +1,78 @@
 <script setup>
+let settings = [
+  {
+    name: 'UniqueIdentifier',
+    type: 'string',
+    isRequired: true,
+    value: ''
+  },
+  {
+    name: 'RomRootFolder',
+    type: 'string',
+    isRequired: true,
+    value: 'z:\\games'          
+  },
+  {
+    name: 'SavesRootFolder',
+    type: 'string',
+    isRequired: false,
+    value: 'z:\\saves'
+  },
+  {
+    name: 'SaveStatesRootFolder',
+    type: 'string',
+    isRequired: false,
+    value: 'z:\\savestates'
+  },
+  {
+    name: 'SendAnalytics',
+    type: 'bool',
+    isRequired: false,
+    value: '1'                  //defaults to 0
+  },
+  {
+    name: 'UseApi',
+    type: 'string',
+    isRequired: false,
+    value: '1'                  //defaults to 0
+  },
+  {
+    name: 'ApiKey',
+    type: 'string',
+    isRequired: false,
+    value: 'asdf'
+  },
+];
 
 </script>
 <template>
     <main>
-        <h1 class="highlight">=== Settings ===</h1>
+        <h1 class='highlight'>=== Settings ===</h1>
+        <div>
+            <ul class="list-group">
+                <li v-for="setting in settings" :key="setting.name" class="list-group-item">
+                    <div class="d-flex align-items-start">
+
+
+                        
+                        <i  class="p-2 bi bi-stoplights"
+                            :class="{   'text-success': !setting.isRequired || (setting.isRequired && setting.value), 
+                                        'text-danger': setting.isRequired && !setting.value }"></i>
+                        <i title="System Status" class="p-2 bi bi-pencil"></i>
+                        <div class="p-2">
+                            {{ setting.name }}
+                        </div>
+                        <div class="p-2">
+                            {{ setting.value }}
+                        </div>
+                    </div>
+
+                </li>
+            </ul>
+        </div>
     </main>
 </template>
+
+<style scoped>
+
+</style>
