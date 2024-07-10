54a2aadf8be9b4bcc1c86da055612dcb752b9178
Shawn Renner <shawn.renner@gmail.com>
2024-07-09 21:44:54 -0500

    create enum-ish class

---
+++ client/RomRepo.service/Workers/WorkerTypeEnum.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomRepo.service.Workers
+{
+    public class WorkerTypeEnum
+    {
+        public string Value { get; private set; }
+
+        private WorkerTypeEnum(string value)
+        {
+            Value = value;
+        }
+
+        public static List<string> GetAll()
+        {
+            // TODO Use reflection to populate list automatically...?
+            var list = new List<string>();
+            list.Add(WorkerTypeEnum.Unpack.Value);
+            list.Add(WorkerTypeEnum.Zip.Value);
+            list.Add(WorkerTypeEnum.Checksum.Value);
+            list.Add(WorkerTypeEnum.Filescan.Value);
+            return list;
+        }
+
+        public static WorkerTypeEnum Unpack
+        {
+            get
+            {
+                return new WorkerTypeEnum("unpack");
+            }
+        }
+
+        public static WorkerTypeEnum Zip
+        {
+            get
+            {
+                return new WorkerTypeEnum("zip");
+            }
+        }
+
+        public static WorkerTypeEnum Checksum
+        {
+            get
+            {
+                return new WorkerTypeEnum("checksum");
+            }
+        }
+
+        public static WorkerTypeEnum Filescan
+        {
+            get
+            {
+                return new WorkerTypeEnum("filescan");
+            }
+        }
+    }
+}
